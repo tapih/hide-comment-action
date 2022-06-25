404310c3fa6e580cb2c163ed719ac0e7d843a86d
Hiroshi Muraoka <h.muraoka714@gmail.com>
2022-06-25 15:45:19 +0900

    Allow non PR events

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { CommentsQuery } from './generated/graphql'
-import { IssueComment } from './generated/graphql-types'
+import { IssueComment, PullRequest } from './generated/graphql-types'
 import { queryComments } from './queries/comments'
 import { minimizeComment } from './queries/minimize'
 
@@ -14,17 +14,14 @@ type Inputs = {
 }
 
 export const run = async (inputs: Inputs): Promise<void> => {
-  if (github.context.payload.pull_request === undefined) {
-    core.info(`non pull_request event: ${github.context.eventName}`)
-    return
-  }
+  const pullRequestNumber = tryGetPullRequestNumber(inputs.pullRequestNumber)
   const octokit = github.getOctokit(inputs.token)
 
-  core.info(`query comments in pull request ${github.context.payload.pull_request.html_url ?? '?'}`)
+  core.info(`query comments in pull request ${pullRequestNumber}`)
   const comments = await queryComments(octokit, {
     owner: github.context.repo.owner,
     name: github.context.repo.repo,
-    number: inputs.pullRequestNumber === null ? github.context.payload.pull_request.number : inputs.pullRequestNumber,
+    number: pullRequestNumber,
   })
 
   const filteredComments = filterComments(comments, inputs)
@@ -32,6 +29,17 @@ export const run = async (inputs: Inputs): Promise<void> => {
     core.info(`minimize comment ${JSON.stringify(c.url)}`)
     await minimizeComment(octokit, { id: c.id })
   }
+}
+
+const tryGetPullRequestNumber = (pullRequestNumber: number | null): number => {
+  if (github.context.payload.pull_request === null) {
+    if (pullRequestNumber === null) {
+      throw new Error(`pull-request-number should be specified`)
+    }
+    return pullRequestNumber
+  }
+  // always use predefined value on pull request trigger
+  return github.context.payload.pull_request.number
 }
 
 type Comment = Pick<IssueComment, 'id' | 'url' | 'isMinimized' | 'author' | 'body'>
